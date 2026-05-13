df205b95139678c627730700ccc77030ca47d59b
gregs650@gmail.com <gregs650@gmail.com>
2026-05-13 16:38:54 -0700

    reorganize files

--- customElement/js/99-test.js
+++ customElement/js/99-test.js
@@ -1,4 +1,5 @@
-<script>
+<script type="module">
+	import { loadJson } from "./whatwg/fetch.js";
 	try {
 		stat.innerHTML += "99-test.js running ....";
 		let data = loadJson('https://raw.githubusercontent.com/sword-2/hvac/refs/heads/main/data/programs/table.office1.json');
