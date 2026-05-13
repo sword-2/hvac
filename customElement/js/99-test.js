19281aebdc7439b696d715bdf1326c8b39231540
gregs650@gmail.com <gregs650@gmail.com>
2026-05-13 16:32:27 -0700

    reorganize files

--- customElement/js/99-test.js
+++ customElement/js/99-test.js
@@ -1,7 +1,7 @@
 <script>
 	try {
 		stat.innerHTML += "99-test.js running ....";
-		data += loadJson('https://raw.githubusercontent.com/sword-2/hvac/refs/heads/main/data/programs/table.office1.json');
+		let data = loadJson('https://raw.githubusercontent.com/sword-2/hvac/refs/heads/main/data/programs/table.office1.json');
 		stat.innerHTML += "<br>data=<br>" + data;
 		} catch(error) {
 			stat.innerHTML += "<br>error=<br>" + error;
